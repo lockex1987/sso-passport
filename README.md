615c044bf9b3216c6b1d25d8c1607239e39e593f
Nguyen Van Huyen <huyennv9@cyberspace.vn>
2021-07-05 09:21:28 +0700

    Khong import ..

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 * Laravel 7 (TODO: nâng cấp lên 9)
 * Không sử dụng built-in authentication để có thể hiểu và tùy chỉnh
 * Dạng SPA, Laravel chỉ làm API
+
+TODO: Danh sách đăng nhập ở thiết bị khác
--- frontend/js/router/routes.js
+++ frontend/js/router/routes.js
@@ -4,48 +4,48 @@ export default [
     {
         path: '/login',
         name: 'login',
-        component: () => import('../pages/Login/LoginIndex.vue')
+        component: () => import('~/pages/Login/LoginIndex.vue')
     },
     {
         path: '/forget-password',
         name: 'forgetPassword',
-        component: () => import('../pages/ForgetPassword/ForgetPasswordIndex.vue')
+        component: () => import('~/pages/ForgetPassword/ForgetPasswordIndex.vue')
     },
     {
         path: '/reset-password',
         name: 'resetPassword',
-        component: () => import('../pages/ResetPassword/ResetPasswordIndex.vue')
+        component: () => import('~/pages/ResetPassword/ResetPasswordIndex.vue')
     },
     {
         path: '/register',
         name: 'register',
-        component: () => import('../pages/Register/RegisterIndex.vue')
+        component: () => import('~/pages/Register/RegisterIndex.vue')
     },
     {
         path: '/verify-email',
         name: 'verifyEmail',
-        component: () => import('../pages/VerifyEmail/VerifyEmailIndex.vue')
+        component: () => import('~/pages/VerifyEmail/VerifyEmailIndex.vue')
     },
 
     {
         path: '/backend',
         name: 'backend',
-        component: () => import('../pages/Backend/BackendIndex.vue'),
+        component: () => import('~/pages/Backend/BackendIndex.vue'),
         children: [
             {
                 path: 'profile',
                 name: 'profile',
-                component: () => import('../pages/Backend/Profile/ProfileIndex.vue')
+                component: () => import('~/pages/Backend/Profile/ProfileIndex.vue')
             },
             {
                 path: 'app',
                 name: 'app',
-                component: () => import('../pages/Backend/App/AppIndex.vue')
+                component: () => import('~/pages/Backend/App/AppIndex.vue')
             },
             {
                 path: 'login-log',
                 name: 'loginLog',
-                component: () => import('../pages/Backend/LoginLog/LoginLogIndex.vue')
+                component: () => import('~/pages/Backend/LoginLog/LoginLogIndex.vue')
             }
         ]
     },
@@ -53,6 +53,6 @@ export default [
     {
         path: '*',
         name: 'NotFound',
-        component: () => import('../pages/NotFound/NotFoundIndex.vue')
+        component: () => import('~/pages/NotFound/NotFoundIndex.vue')
     }
 ];
